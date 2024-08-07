63f59004789128351f28b5c5ba1bf68099cfdf10
Samuel <107401160+SamIxCode@users.noreply.github.com>
2024-08-07 15:35:25 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Main } from '@/components/main'
+import { Minv3 } from '@/components/minv3'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><Minv3 /></div>
+  )
+}
+
+export default page
---
+++ components/navigation/nav.tsx
@@ -0,0 +1,33 @@
+import { MountainIcon } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const nav = () => {
+    return (
+        <div>
+
+            <header className="px-4 lg:px-6 h-14 flex items-center">
+                <Link href="#" className="flex items-center justify-center" prefetch={false}>
+                    <MountainIcon className="h-6 w-6" />
+                    <span className="sr-only">Acme Web Development</span>
+                </Link>
+                <nav className="ml-auto flex gap-4 sm:gap-6">
+                    <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                        Services
+                    </Link>
+                    <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                        Portfolio
+                    </Link>
+                    <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                        About
+                    </Link>
+                    <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                        Contact
+                    </Link>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default nav
